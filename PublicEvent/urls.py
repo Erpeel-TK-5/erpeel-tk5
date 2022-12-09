01af3734c8fb2dd4393ab530311b12cdcb935b18
haseaaaa <denny.johannes@ui.ac.id>
2022-12-09 16:33:27 +0700

    Menyelesaikan fitur Public event dengan UI minimalis

--- PublicEvent/urls.py
+++ PublicEvent/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='public_event'),
+    path('<str:nama>/', views.user_public_event, name="user_public_event")
 ]
--- PublicEvent/views.py
+++ PublicEvent/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'public_event.html')
+
+def user_public_event(request, nama):
+    return render(request, "user_public_event.html", {"nama":nama})

458f525a4ef093f4fc5dce6ceb819043625f8e2e
Razita Afrina Nuriyan <razita.afrina@gmail.com>
2022-12-10 15:54:36 +0700

    Add localization for homepage & auth pages

--- authentication/views.py
+++ authentication/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib import messages
 from django.http.response import HttpResponse, HttpResponseRedirect
+from django.utils.translation import get_language
 
 
 def index(request):
     if "isLogin" in request.session:
-        return redirect('/') #ganti sama url yg nampilin display kalo user ud login
+        return redirect('/calendar') #ganti sama url yg nampilin display kalo user ud login
     else:
-        return render(request, 'auth.html')
+        if get_language == 'id':
+            return render (request, 'auth.html')
+        return render(request, 'auth-en.html')
 
 def login(request):
     if "isLogin" in request.session:
-        return redirect('/') #ganti sama url yg nampilin display kalo user ud login
+        return redirect('/calendar') #ganti sama url yg nampilin display kalo user ud login
     response={}
     response['error'] = False
     form = LoginForm(request.POST or None)
@@ -23,23 +26,27 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             headers = {"Authorization": "Bearer fdca0f928bbcc40b67191f73f381c15b9b30cb5a8befe6facb62c503d4d24af3e9c0d1df7a1e7f448c9a992bc7e44d9b905158a602fa68f75ffdd740bc09a9ba342012eb35dda37ea89bba1b28399929b679c28487542aa5f041fa1eb1a3ee323ed6e234ec9e637444426a2b72f0ae336bd1e9e76aa6753c1a97b549c564efdc"}
-            api_response = requests.get('https://strapi-production-ef0a.up.railway.app/api/calendar-users?fields[0]=username&fields[1]=email&fields[2]=password', headers=headers).json()
+            api_response = requests.get('https://strapi-production-ef0a.up.railway.app/api/calendar-users?fields[0]=username&fields[1]=email&fields[2]=password&fields[3]=bahasa', headers=headers).json()
             if api_response.get('error') != None:
                 error_list = api_response.get('error').get('details').get('errors')
                 error_messages = ""
                 for error in error_list:
                     error_messages += error.get('message') + ", "
                 messages.info(request, error_messages[:-2])
-                return render(request, 'login.html', response)
+                if (get_language == 'id'):
+                    return render(request, 'login.html', response)
+                return render(request, 'login-en.html', response)
             username_list = []
             email_list = []
             password_list = []
+            lang_list = []
             id_list = []
             for user in api_response.get('data'):
                 attrs = user.get('attributes')
                 username_list.append(attrs.get('username'))
                 email_list.append(attrs.get('email'))
                 password_list.append(attrs.get('password'))
+                lang_list.append(attrs.get('bahasa'))
                 id_list.append(user.get('id'))
             index = -1
             if username in username_list:
@@ -51,17 +58,24 @@ def login(request):
                 return render(request, 'login.html', response)
             if index != -1 and password == password_list[index]:
                 request.session['uid'] = id_list[index]
+                request.session['bahasa'] = lang_list[index]
                 request.session['isLogin'] = True
-                return redirect('/') #ganti sama url yg nampilin display kalo user ud login
+                return redirect('/calendar') #ganti sama url yg nampilin display kalo user ud login
             else:
                 messages.info(request, 'Password salah')
-            return render(request, 'login.html', response)
+            if (get_language == 'id'):
+                return render(request, 'login.html', response)
+            return render(request, 'login-en.html', response)
         else:
-            return render(request, 'login.html', response)
+            if (get_language == 'id'):
+                return render(request, 'login.html', response)
+            return render(request, 'login-en.html', response)
     else:
         form = LoginForm(request.POST or None)
         response['form'] = form
-        return render(request,'login.html',response)
+        if (get_language == 'id'):
+            return render(request, 'login.html', response)
+        return render(request, 'login-en.html', response)
 
 def logout(request):
     if "uid" in request.session:
@@ -71,7 +85,7 @@ def logout(request):
 
 def register(request):
     if "isLogin" in request.session:
-        return redirect('/') #ganti sama url yg nampilin display kalo user ud login
+        return redirect('/calendar') #ganti sama url yg nampilin display kalo user ud login
     response={}
     response['error'] = False
     form = RegisterForm(request.POST or None)
@@ -82,8 +96,9 @@ def register(request):
              password = request.POST['password']
              email = request.POST['email']
              name = request.POST['name']
+             lang = get_language()
              headers = {"Authorization": "Bearer fdca0f928bbcc40b67191f73f381c15b9b30cb5a8befe6facb62c503d4d24af3e9c0d1df7a1e7f448c9a992bc7e44d9b905158a602fa68f75ffdd740bc09a9ba342012eb35dda37ea89bba1b28399929b679c28487542aa5f041fa1eb1a3ee323ed6e234ec9e637444426a2b72f0ae336bd1e9e76aa6753c1a97b549c564efdc"}
-             data = {"data": {"username": username, "password": password, "email": email, "nama": name}}
+             data = {"data": {"username": username, "password": password, "email": email, "nama": name, "bahasa": lang}}
              api_response = requests.post('https://strapi-production-ef0a.up.railway.app/api/calendar-users', json=data, headers=headers).json()
              if api_response.get('error') != None:
                 error_list = api_response.get('error').get('details').get('errors')
@@ -94,13 +109,20 @@ def register(request):
                     else:
                         error_messages += error.get('message') + ", "
                 messages.info(request, error_messages[:-2])
-                return render(request, 'register.html', response)
+                if (get_language == 'id'):
+                    return render(request, 'register.html', response)
+                return render(request, 'register-en.html', response)
              request.session['uid'] = api_response.get('data').get('id')
+             request.session['bahasa'] = lang
              request.session['isLogin'] = True
-             return redirect('/') #ganti sama url yg nampilin display kalo user ud login
+             return redirect('/calendar') #ganti sama url yg nampilin display kalo user ud login
         else:
-            return render(request, 'register.html', response)
+            if (get_language == 'id'):
+                return render(request, 'register.html', response)
+            return render(request, 'register-en.html', response)
     else:
         form = RegisterForm(request.POST or None)
         response['form'] = form
-        return render(request,'register.html',response)
+        if (get_language == 'id'):
+            return render(request, 'register.html', response)
+        return render(request, 'register-en.html', response)

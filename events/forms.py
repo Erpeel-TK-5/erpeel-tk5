757c87611a01f906e86fe211d28d68127212ae9f
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2022-12-10 19:24:27 +0700

    Add create events

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
-# class EventForm(forms.Form):
-#     name=forms.CharField(label='Username/Email',max_length=50)
-#     password=forms.CharField(label='Password',widget=forms.PasswordInput, max_length = 50)
+class EventForm(forms.Form):
+    name=forms.CharField(label='Nama-Event',max_length=50)
+    start_date=forms.DateField(label='Start-Date',widget=forms.DateInput(attrs={'type': 'date'}))
+    end_date=forms.DateField(label='End-Date',widget=forms.DateInput(attrs={'type': 'date'}))
+    is_public=forms.BooleanField(label='Is-Public',widget=forms.CheckboxInput)
+    is_recurring=forms.BooleanField(label='Is-Recurring',widget=forms.CheckboxInput)
+    notes=forms.CharField(label='Notes',max_length=1000)

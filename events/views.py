15aeb95ff7189ef27ebc8eb13652ec616ebf2bae
ferdinandg <frferdinand22@gmail.com>
2022-12-12 17:16:00 +0700

    Fix public and recurring required to check when making new event

--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ import requests
 from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib import messages
+from django.utils.datastructures import MultiValueDictKeyError
 from django.http.response import HttpResponse, HttpResponseRedirect
 
 def index(request):
@@ -21,8 +22,17 @@ def create_event(request) :
                 name = request.POST['name']
                 start_date = request.POST['start_date']
                 end_date = request.POST['end_date']
-                is_public = True if request.POST['is_public']=="on" else False
-                is_recurring = True if request.POST['is_recurring']=="on" else False
+
+                try:
+                    is_public = True if request.POST['is_public']=="on" else False
+                except MultiValueDictKeyError:
+                    is_public = False
+
+                try:
+                    is_recurring = True if request.POST['is_recurring']=="on" else False
+                except MultiValueDictKeyError:
+                    is_recurring = False
+
                 dibuat_oleh = request.session['uid']
                 notes = request.POST['notes']
                 headers = {"Authorization": "Bearer fdca0f928bbcc40b67191f73f381c15b9b30cb5a8befe6facb62c503d4d24af3e9c0d1df7a1e7f448c9a992bc7e44d9b905158a602fa68f75ffdd740bc09a9ba342012eb35dda37ea89bba1b28399929b679c28487542aa5f041fa1eb1a3ee323ed6e234ec9e637444426a2b72f0ae336bd1e9e76aa6753c1a97b549c564efdc"}
